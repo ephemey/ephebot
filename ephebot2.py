6e05188dde41813c69382ffd9cc5e9319229a19c
ephy <rhakumei@gmail.com>
2018-02-03 03:22:10 +0000

    i gib up

--- ephebot2.py
+++ ephebot2.py
@@ -10,15 +10,25 @@ async def on_ready():
     print("Username: " + bot.user.name)
     print("ID: " + bot.user.id)
 
-@bot.event
-async def on_message(message):
-    if message.content == "cookie":
-        await bot.send_message(message.channel, ":cookie:")
+# @bot.event
+# async def on_message(message):
+#     if message.content == "cookie":
+#         await bot.send_message(message.channel, ":cookie:")
 
 @bot.command(pass_context=True)
 async def ping(ctx):
     await bot.say(":ping_pong: pong!!")
     print("user has pinged")
+
+@bot.command(pass_context=True)
+async def meow(ctx):
+    await bot.say("nyaaaa")
+    print("nyaaaaa")
+
+@bot.command(pass_context=True)
+async def nsfw(ctx):
+    await bot.say("WARNING NSFW WARNING NSFW WARNING NSFW WARNING NSFW WARNING NSFW")
+    print("NSFW af")
 
 # @bot.command(pass_context=True)
 # async def kick(ctx, user: discord.Member):
