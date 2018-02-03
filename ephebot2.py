5b9d97b7a9b15d48c93bf76972bd82b83c3b5bdf
ephy <rhakumei@gmail.com>
2018-02-03 03:17:44 +0000

    y am i stil here

--- ephebot2.py
+++ ephebot2.py
@@ -10,6 +10,11 @@ async def on_ready():
     print("Username: " + bot.user.name)
     print("ID: " + bot.user.id)
 
+@bot.event
+async def on_message(message):
+    if message.content == "cookie":
+        await bot.send_message(message.channel, ":cookie:")
+
 @bot.command(pass_context=True)
 async def ping(ctx):
     await bot.say(":ping_pong: pong!!")
@@ -19,12 +24,6 @@ async def ping(ctx):
 # async def kick(ctx, user: discord.Member):
 #     await bot.say(":boot: Bye, {}.".format(user.name))
 #     await bot.kick(user)
-
-# @bot.event
-# async def on_message(message):
-#     if message.content == "cookie":
-#         print('cookie')
-#         await bot.say(message.channel, ":cookie:")
 
 
 bot.run("NDA4NDYwOTU0NzgzNzExMjMy.DVadMQ.XR_SaTOX9DzljLGtOr5CV1MoN80")
