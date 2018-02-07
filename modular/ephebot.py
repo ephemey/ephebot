70be547215d80bf434274b2c3dce7d5001e3da80
ephy <rhakumei@gmail.com>
2018-02-07 20:30:15 +0000

    + load, unload, reload, ask for token and stuff

--- modular/ephebot.py
+++ modular/ephebot.py
@@ -2,9 +2,14 @@ import discord
 from discord.ext import commands
 from discord.ext.commands import Bot
 import asyncio
+import importlib
+
+# logging
+logging.basicConfig(level=logging.WARNING)
 
 # ask for token
-token = input('bot token: ')
+# token = input('bot token: ')
+token = "NDA4NDYwOTU0NzgzNzExMjMy.DVzywA.27Ee8-s06s7PUW7SVHBtpibQ2s4"
 
 # set bot commmand prefix
 bot = commands.Bot(command_prefix=';')
@@ -22,5 +27,29 @@ async def load(arg):
     await import arg
     await bot.say("Loaded " + arg)
     print("Loaded " + arg)
+
+    # something something module not found exception
+
+# unload module
+@bot.command()
+async def unload(arg):
+    await del arg
+    await bot.say(arg + "unloaded")
+    print(arg + "unloaded")
+
+    # something something module not loaded exception
+
+#reload module
+@bot.command()
+async def reload(arg):
+    await importlib.reload(arg)
+    await bot.say("Reloaded " + arg)
+    print("Reloaded " + arg)
+
+    # something something module not found exception
+
+# bot restart command ..?
+
+
 
 bot.run(token)
